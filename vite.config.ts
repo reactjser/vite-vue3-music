aeaef07b91ead305dddbcbf055c9895d2234f6f2
reactjser <reactjser@163.com>
2021-08-13 22:20:19 +0800

    feat: init project

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '/@': resolve(__dirname, './src'),
+    },
+  },
+});
